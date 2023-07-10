5721ebbcc5a5d03fc9e7b0e3209264a06c84c237
Doug Parker <dgp1130@users.noreply.github.com>
2023-07-10 16:32:38 -0700

    Fix warnings.

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,6 @@
-// jest.config.js
-module.exports = {
+export default {
   preset: 'jest-preset-angular/presets/defaults-esm',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   moduleNameMapper: {
     rxjs: '<rootDir>/node_modules/rxjs/dist/bundles/rxjs.umd.js',
   },
